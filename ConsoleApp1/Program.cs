4c0810cc36b595d2ecc964fe7d14fbd7e8ec22bb
habiba x <habiba20191700207@cis.asu.edu.eg>
2022-05-21 13:07:16 +0200

    complexity added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,245 +5,252 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 using System.IO;
-using System.Numerics;
-
+using System.Diagnostics;
 class graph
 {
-    public static Dictionary<Tuple<int, int>, int> we = new Dictionary<Tuple<int, int>, int>(); //to minize memory by half, we order pairs value before add or get
-    public static List<HashSet<int>> adj = new List<HashSet<int>>();         //vertex will be converted to int
-    public static Dictionary<string, int> map = new Dictionary<string, int>();
-    public static Dictionary<int, string> revMap = new Dictionary<int, string>();// we can use it as an array not dictionary
-    public static int[] level;
-    public static List<List<int>> lvl;
-    public static int[] rsw;
+    public static Dictionary<Tuple<int, int>, int> weight = new Dictionary<Tuple<int, int>, int>();//O(1)
+    public static List<ArrayList> adj = new List<ArrayList>();//O(1)
+    public static Dictionary<Tuple<int, int>, string> actor_to_movie = new Dictionary<Tuple<int, int>, string>();//O(1)
+    public static Dictionary<string, int> map = new Dictionary<string, int>();//O(1)
+    public static Dictionary<int, string> revMap = new Dictionary<int, string>();//O(1)
     static public void Main(String[] args)
     {
+        Stopwatch sw = Stopwatch.StartNew();
         read();
-        Console.WriteLine("finished");
+        sw.Stop();
+        Console.WriteLine("\n");//O(1)
+        Console.WriteLine("finished in \r");//O(1)
+        Console.WriteLine(sw.Elapsed.ToString());//O(1)
     }
-    static void read()
+
+    static void read()//O(movies*V^2)
     {
-        string text = File.ReadAllText(@"Movies122806.txt");
-        string[] movies = text.Split('\n');
-        string[][] actors = new string[movies.Length][];
-        Tuple<int, int> tuple;
-        for (int i = 0; i < movies.Length; i++)
-        {
-            actors[i] = movies[i].Split('/');
+        //sample
+        string text = File.ReadAllText(@"movies1.txt");//O()                                                                
+        //small case1
+        //string text = File.ReadAllText(@"Movies193.txt");//O(1)
+        //small case2
+        //string text = File.ReadAllText(@"Movies187.txt");//O(1)
+        //medium case1
+        //string text = File.ReadAllText(@"Movies967.txt");//O(1)
+        //medium case2
+        //string text = File.ReadAllText(@"Movies4736.txt");//O(1)
+        //large
+        //string text = File.ReadAllText(@"Movies14129.txt");//O(1)
+        //Extreme
+        // string text = File.ReadAllText(@"Movies122806.txt");//O(1)
 
+        string[] movies = text.Split('\n');//O(n) where n num of characters in string 
+        string[][] actors = new string[movies.Length][];//O(1)
+        Tuple<int, int> tuple;//O(1)
+        for (int i = 0; i < movies.Length; i++)//O(movies*n)
+        {
+            actors[i] = movies[i].Split('/');//O(n) where n num of characters in string 
         }
-        for (int i = 0; i < movies.Length; i++)
+
+        for (int i = 0; i < movies.Length; i++)//O(movies*V^2)
         {
-            int length = actors[i].Length;
-            for (int j = 1; j < length; j++)
+            int length = actors[i].Length;//O(1)
+            for (int j = 1; j < length; j++)//O(V)
             {
-                if (!map.ContainsKey(actors[i][j]))
+                if (!map.ContainsKey(actors[i][j]))//O(1)
                 {
-                    adj.Add(new HashSet<int>());
-                    map.Add(actors[i][j], map.Count);
-                    revMap.Add(revMap.Count, actors[i][j]);
-
-
+                    map.Add(actors[i][j], map.Count);//O(1) Till finishing capacity becomes O(V)
+                    adj.Add(new ArrayList());//O(1)
+                    revMap.Add(revMap.Count, actors[i][j]);//O(1) Till finishing capacity becomes O(V)
                 }
-                for (int k = 1; k < length; k++)
+                for (int k = 1; k < length; k++)//O(V)
                 {
-                    //Console.Write("  " + j);
-
-                    if (!map.ContainsKey(actors[i][k]))
+                    if (!map.ContainsKey(actors[i][k]))//O(1)
                     {
-                        adj.Add(new HashSet<int>());
-                        map.Add(actors[i][k], map.Count);
-                        revMap.Add(revMap.Count, actors[i][k]);
-
+                        adj.Add(new ArrayList());//O(1)
+                        map.Add(actors[i][k], map.Count);//O(1) Till finishing capacity becomes O(V)
+                        revMap.Add(revMap.Count, actors[i][k]);//O(1) Till finishing capacity becomes O(V)
                     }
-                    if (k != j)
+                    if (k != j)//O(1)
                     {
-                        //Console.WriteLine(map[actors[i][j]]);
-                        adj[(map[actors[i][j]])].Add(map[actors[i][k]]);
+                        adj[(map[actors[i][j]])].Add(map[actors[i][k]]);//O(1) Till finishing capacity becomes O(V)
                     }
                 }
             }
-            for (int j = 1; j < length; j++)
+
+
+            for (int j = 1; j < length; j++)//O(V^2)
             {
-                for (int k = j + 1; k < length; k++)
+                for (int k = j + 1; k < length; k++)//O(V)
                 {
-                    int min = Math.Min(map[actors[i][j]], map[actors[i][k]]);
-                    int max = Math.Max(map[actors[i][j]], map[actors[i][k]]);
-                    tuple = new Tuple<int, int>(min, max);
-                    if (we.ContainsKey(tuple))
+                    int min = Math.Min(map[actors[i][j]], map[actors[i][k]]);//O(1)
+                    int max = Math.Max(map[actors[i][j]], map[actors[i][k]]);//O(1)
+                    tuple = new Tuple<int, int>(min, max);//O(1)
+
+                    if (weight.ContainsKey(tuple))//O(1)
                     {
-                        we[tuple]++;
+                        weight[tuple]++;//O(1)
                     }
                     else
                     {
-                        we.Add(tuple, 1);
+                        weight.Add(tuple, 1);//O(1)
                     }
-
+                    if (!actor_to_movie.ContainsKey(tuple))//O(1)
+                    {
+                        actor_to_movie.Add(tuple, actors[i][0]);//O(1) Till finishing capacity becomes O(V)
+                    }
                 }
             }
         }
+        //sample
+        string text2 = File.ReadAllText(@"queries1.txt");
+        //small case1
+        //string text2 = File.ReadAllText(@"queries110.txt");
+        //small case2
+        //string text2 = File.ReadAllText(@"queries50.txt");
+        //medium case1 85
+        //string text2 = File.ResadAllText(@"queries85.txt");
+        //medium case1 4000
+        //string text2 = File.ReadAllText(@"queries4000.txt");
+        //medium case2 110
+        //string text2 = File.ReadAllText(@"queries110.txt");
+        //medium case2 2000
+        //string text2 = File.ReadAllText(@"queries2000.txt");
+        //large 26
+        //string text2 = File.ReadAllText(@"queries26.txt");
+        //large 600
+        //string text2 = File.ReadAllText(@"queries600.txt");
+        //Extreme200
+        //string text2 = File.ReadAllText(@"queries200.txt");
+        //Extreme22
+        // string text2 = File.ReadAllText(@"queries22.txt");
 
-
-
-
-        string text2 = File.ReadAllText(@"queries22.txt");
-        string[] queries = text2.Split('\n');
-        text2 = File.ReadAllText(@"queries22 - Solution.txt");
-        string[] sol = text2.Split('\n');
-        level = new int[(map.Count + 1)];
-        rsw = new int[map.Count + 1];
-        for (int i = 0; i < queries.Length - 1; i++)
+        string[] queries = text2.Trim().Split('\n');//O(n) Where n numbers of letters per line
+        for (int i = 0; i < queries.Length; i++)//O(M) where M number of queries
         {
-
-            string[] values = queries[i].Trim().Split('/');
-            int src = map[values[0]];
-            int dst = map[values[1]];
-            rsw = new int[level.Length];
-            bfs(src, dst);
-
-            rs(src, dst);
-            string[] sol2 = sol[1 + 5 * i].Trim().Split();
-            int x = Int32.Parse(sol2[2].Substring(0, sol2[2].Length - 1));
-            int y = Int32.Parse(sol2[5].Substring(0, sol2[5].Length));
-
-            if (x == (level[dst] - 1) && y == rsw[src])
-            {
-                Console.WriteLine("Case: " + i + " Accepted");
-            }
-            else
-            {
-                Console.WriteLine("soruce: " + src + " dest " + dst);
-                Console.WriteLine("output: " + (level[dst] - 1) + " " + rsw[dst]);
-                Console.WriteLine("output: source " + (level[src] - 1) + " " + rsw[src]);
-
-                Console.WriteLine("outpu: " + x + " " + y);
-                Console.WriteLine("Wrong Answer Case: " + i);
-            }
-
-        }
-        Console.WriteLine(" \n \n Finished");
-    }
-    static void bfs(int source, int dest)
-    {
-        bool[] visited = new bool[level.Length];
-        Queue<int> q = new Queue<int>();
-        level = new int[level.Length];
-        lvl = new List<List<int>>();
-        lvl.Add(new List<int>());
-        lvl[0].Add(source);
-        //created after doing the bfs or while doing it
-
-        level[source] = 1;
-        visited[source] = true;
-        q.Enqueue(source);
-        while (q.Count != 0)
-        {
-            int v = q.Dequeue();
-            if (level[v] >= level[dest] && level[dest] != 0)
-            {
-                break;
-            }
-            for (int i = 0; i < adj[v].Count; i++)
-            {
-
-                int index = adj[v].ElementAt(i);
-
-                /*int x;
-                if (v > index) { x = rsw[v] + we[new Tuple<int, int>(index, v)]; }
-                else { x = rsw[v] + we[new Tuple<int, int>(v, index)]; }*/
-                if (!visited[index] && level[dest] == 0)
-                {
-                    if (lvl.Count < level[v] + 1) { lvl.Add(new List<int>()); }
-                    lvl[level[v]].Add(index);
-                    level[index] = level[v] + 1;
-                    visited[index] = true;
-                    q.Enqueue(index);
-                }
-
-                /*if (rsw[index] < x && level[v] == level[index] - 1)
-                {
-                    rsw[index] = x;
-                }*/
-
-            }
-
-        }
-
-    }
-    static void rs(int source, int dest)
-    {
-        Queue<int> q = new Queue<int>();
-        List<int> visited = new List<int>();
-        q.Enqueue(dest);
-        while (q.Count != 0)
-        {
-            int v = q.Dequeue();
-            if (v == source) { break; }
-            Console.WriteLine(lvl[v].Count + " ");
-            for (int i = 0; i < lvl[level[v] - 2].Count; i++)
-            {
-                int u = (lvl[level[v] - 2][i]);
-                if (adj[v].Contains(u))
-                {
-
-                    if (!visited.Contains(u) && !q.Contains(u))
-                    {
-                        q.Enqueue(u);
-                        visited.Add(u);
-                    }
-                    int x;
-                    if (v > u) { x = rsw[v] + we[new Tuple<int, int>(u, v)]; }
-                    else { x = rsw[v] + we[new Tuple<int, int>(v, u)]; }
-                    if (rsw[u] < x)
-                    {
-                        rsw[u] = x;
-                    }
-                }
-
-
-
-
-
-
-                /*
-                int index = adj[v].ElementAt(i);
-
-                if (lvl[level[v]-2].Contains(index)){
-
-                    if (!visited.Contains(index) &&!q.Contains(index)) {
-                        q.Enqueue(index);
-                        visited.Add(index);
-                    }
-
-                    int x;
-                    if (v > index) { x = rsw[v] + we[new Tuple<int, int>(index, v)]; }
-                    else { x = rsw[v] + we[new Tuple<int, int>(v, index)]; }
-                    if (rsw[index] < x )
-                    {
-                        rsw[index] = x;
-                       // Console.WriteLine(index + " " + x);
-                    }
-                }
-            */
-            }
+            string[] values = queries[i].Trim().Split('/');//O(n) complexity where n is the number of characters in the string.
+            int src = map[values[0]];//O(1)
+            int dst = map[values[1]];//O(1)
+            int[] level = new int[map.Count];//O(1)
+            level = dos(src, dst);//O(V+E)
+            Console.WriteLine(revMap[src] + "/" + revMap[dst]);//O(1)
+            Console.WriteLine("dos = " + (level[dst] - 1));//O(1)
+            rs(src, dst, level);//O(V+E)
         }
     }
-}
 
-struct edge
-{
-    public int v1;
-    public int v2;
-}
-struct Pair
-{
-    public int v;
-    public int i;
-    private int dst;
-
-    public Pair(int dst, int i) : this()
+    static int[] dos(int src, int dist)// O(V+E)
     {
-        this.dst = dst;
-        this.i = i;
+        Queue<int> q = new Queue<int>();// O(1)
+        int[] level = new int[map.Count];// θ(1)
+        q.Enqueue(src);// O(1)
+
+        level[src] = 1;// O(1)
+
+        while (q.Count != 0)// O(V)
+        {
+            if (level[dist] == 0)//O(1)
+            {
+                int v = q.Dequeue();//O(1)
+                for (int i = 0; i < adj[v].Count; i++)//O(E)
+                {
+                    int index = (int)adj[v][i];//O(1)
+                    if (level[index] == 0)//O(1)
+                    {
+                        q.Enqueue(index);//O(1)
+                        level[index] = level[v] + 1;//O(1)
+                    }
+                }
+            }
+            else return level;//O(1)
+        }
+
+        return level;//O(1)
+    }
+
+
+    static void rs(int source, int destination, int[] level)//O(V+E)
+    {
+        HashSet<int> visited = new HashSet<int>();//O(1)
+        Tuple<int, int> e1;//O(1)
+        Tuple<int, int> e3;//O(1)
+        Queue<int> q = new Queue<int>();//O(1)
+        int[] parent = new int[level.Length];// O(1)
+        int[] distance = new int[level.Length];//O(1)
+        q.Enqueue(destination);//O(1)
+        visited.Add(destination);//O(1) Till finishing capacity becomes O(V)
+        while (q.Count != 0)//O(V)
+        {
+            int v = q.Dequeue();//O(1)
+            for (int i = 0; i < adj[v].Count; i++)//O(V+E)
+            {
+                int index = (int)adj[v][i];//O(1)
+                if (level[index] != 0 && level[index] < level[v])//O(1)
+                {
+                    if (level[index] != level[source])//O(1)
+                    {
+                        if (!visited.Contains(index))//O(1)
+                        {
+                            q.Enqueue(index);//O(1)
+                            visited.Add(index);//O(1) 
+                        }
+                    }
+                    int min = Math.Min(destination, index);//O(1)
+                    int max = Math.Max(destination, index);//O(1)
+                    e1 = new Tuple<int, int>(min, max);
+                    if (v != destination)//O(1)
+                    {
+                        int min2 = Math.Min(index, v);//O(1)
+                        int max2 = Math.Max(index, v);//O(1)
+                        e3 = new Tuple<int, int>(min2, max2);//O(1)
+                        int w = distance[v] + weight[e3];//O(1)
+                        if (w > distance[index])
+                        {
+                            distance[index] = w;//O(1)
+                            parent[index] = v;
+                        }//O(1)
+                    }
+                    else
+                    {
+                        distance[index] = weight[e1];//O(1)
+                        parent[index] = v;//O(1)
+                    }
+                }
+            }
+        }
+        Console.WriteLine("rs = " + distance[source]);//O(1)
+
+        chains(source, destination, parent);//O(V)
+    }
+
+
+    static void chains(int source, int destination, int[] parent)//O(V)
+    {
+        Console.Write("chain of movies = ");//O(1)
+        Queue<int> m = new Queue<int>();//O(1)
+        m.Enqueue(source);//O(1)
+        while (m.Count > 0)//O(1)
+        {
+            int d = m.Dequeue();//O(1)
+            int min = Math.Min(d, parent[d]);//O(1)
+            int max = Math.Max(d, parent[d]);//O(1)
+            Tuple<int, int> e = new Tuple<int, int>(min, max);//O(1)
+            Console.Write(actor_to_movie[e]);//O(1)
+            if (parent[d] != destination)//O(1)
+            {
+                Console.Write("=>");//O(1)
+                m.Enqueue(parent[d]);//O(1)
+            }
+        }
+        Console.WriteLine();//O(1)
+        Console.Write("chain of actors = ");//O(1)
+        Queue<int> a = new Queue<int>();//O(1)
+        a.Enqueue(source);
+        while (a.Count > 0) //O(V)
+        {
+            int d = a.Dequeue();//O(1)
+            Console.Write(revMap[d] + " -> ");
+            if (parent[d] != destination)//O(1)
+            {
+                a.Enqueue(parent[d]);//O(1)
+            }
+        }
+        Console.WriteLine(revMap[destination]);//O(1)
     }
 }
